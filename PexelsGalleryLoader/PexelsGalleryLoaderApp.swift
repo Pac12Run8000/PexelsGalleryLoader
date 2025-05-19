dc499df7efdc91c88f956506c79feb0a16b229ce
Norbert Grover <norbert298@gmail.com>
2025-05-18 20:45:27 -0700

    Initial Commit

---
+++ PexelsGalleryLoader/PexelsGalleryLoaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  PexelsGalleryLoaderApp.swift
+//  PexelsGalleryLoader
+//
+//  Created by Norbert Grover on 5/18/25.
+//
+
+import SwiftUI
+
+@main
+struct PexelsGalleryLoaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
