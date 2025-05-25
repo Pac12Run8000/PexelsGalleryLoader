b7e6c32d42da16f5635b546cd54d3314efeffbdf
Norbert Grover <norbert.grover@joinklover.com>
2025-05-25 08:52:24 -0700

    Update SecurityValues.swift

--- PexelsGalleryLoader/SecurityConstants/SecurityValues.swift
+++ PexelsGalleryLoader/SecurityConstants/SecurityValues.swift
@@ -2,6 +2,6 @@ import Foundation
 
 enum SecurityValues {
     enum APIKey {
-        static let key = "EOc5iLsA1NotO4AlHTA7KiXbP2UhBr8LF362YBi4C71pyUnJdl3Na7e1"
+        static let key = "API key"
     }
 }
